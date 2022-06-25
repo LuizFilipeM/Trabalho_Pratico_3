8e2df3a72d231bef2423ac3bf4a2f8662b4fd4ea
Luiz_F <103970058+LuizFilipeM@users.noreply.github.com>
2022-06-25 18:36:03 -0300

    Create ShiftAndExato.h

---
+++ ShiftAndExato.h
@@ -0,0 +1,10 @@
+#include "Musica.h"
+
+typedef struct mascara{
+    int masc;
+    int *potencias;
+} mascara;
+
+mascara* cria_mascaras(sequencia* , sequencia* , mascara*);
+int shift_and_exato(sequencia* , mascara* , int);
+void limpeza_mascaras(mascara* , int);
