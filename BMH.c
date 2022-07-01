e4b383d30e12c6a0192463b434bac5760372a7e4
Luiz_F <103970058+LuizFilipeM@users.noreply.github.com>
2022-07-01 20:33:42 -0300

    Update BMH.c

--- BMH.c
+++ BMH.c
@@ -1,54 +1,63 @@
 #include "BMH.h"
+
+// para padronização o texto será chamado de arranjo1 e o padrão de arranjo2
+
+// nosso alfabeto é sempre as notas musicais, por isso defnimos o tamanho 12
 #define tam_alfabet 12
 
+
 int BMH(sequencia *arranjo1, sequencia *arranjo2){
+
+    // caso o tamanho  do padrão e do texto sejam iguais a 0, a função 
+    //já retorna um valor que encerra o programa
     if((arranjo1->tamanho == 0) && (arranjo2->tamanho == 0)) return -7;
-    int tab_desloc[tam_alfabet];
-    
-    for (int i = 0; i < tam_alfabet; i++){
-        tab_desloc[i] = arranjo2->tamanho;
-    }
-    
-    for (int i = 1; i < arranjo2->tamanho; i++){
-        tab_desloc[arranjo2->notas[i-1]] = (arranjo2->tamanho) - i;
-    }   
+
+    int *tab_desloc;
+    int tab_aux[tam_alfabet];
+    tab_desloc = faz_tab_desloc(arranjo2, tab_aux); // inicializa a tabela de deslocamento do metodo BMH
     
     int i = (arranjo2->tamanho), j, k;
     int potencia_ant, potencia_atual;
     k = i;
     j = arranjo2->tamanho;
-        while (i <= arranjo1->tamanho)
-        {
-           
-            if(comparador(arranjo1->notas[k-1],arranjo2->notas[j-1]) == 0){
-                k = i;
-                j = arranjo2->tamanho;
-                while (arranjo1->notas[k-1] == arranjo2->notas[j-1]){
-                    k--;
-                    j--;
-                    if (j == 0) return k;
-                }
-                
-                
-            } else{
-                k = i;
-                j = arranjo2->tamanho;
-                potencia_atual = comparador(arranjo1->notas[k-1], arranjo2->notas[j-1]);
-                potencia_ant = potencia_atual;
-                
-                while (potencia_ant == potencia_atual && j > 0 && potencia_atual != -6){
-                    k--;
-                    j--;
-                    if (j == 0) return k;
-                    potencia_atual = comparador(arranjo1->notas[k-1], arranjo2->notas[j-1]);
-                }
-                                    
-            }
 
-            if (i == (arranjo1->tamanho)) i = 1 + (arranjo1->tamanho); 
-            else i = i + tab_desloc[arranjo1->notas[i-1]];
-        }
-
+    while (i <= arranjo1->tamanho) //enquanto i for menor que o tamanho do texto pode haver casamento
+    {
+        k = i;
+        j = arranjo2->tamanho;
+        potencia_atual = comparador(arranjo1->notas[k-1], arranjo2->notas[j-1]);
+        potencia_ant = potencia_atual;
         
+        while (potencia_ant == potencia_atual && j > 0 && potencia_atual != -6){
+            k--;
+            j--;
+            if (j == 0) return k;
+            potencia_atual = comparador(arranjo1->notas[k-1], arranjo2->notas[j-1]);
+        }
+                
+        // no caso de já ter computado a última posição do texto
+        // e não ter encontrado uma ocorrencia, sai do while
+        if (i == (arranjo1->tamanho)) break;
+        else i = i + tab_desloc[arranjo1->notas[i-1]];
+    }
+     
     return -6;
+}
+
+
+int* faz_tab_desloc(sequencia *padrao, int* tab_desloc){
+    
+    // inicializa a tabela de deslocamento com todas as posições recebendo
+    // o tamanho do padrão para não haver posições inválidas
+    for (int i = 0; i < tam_alfabet; i++){
+        tab_desloc[i] = padrao->tamanho;
+    }
+    
+    
+    //faz o cálculo dos deslocamentos e os atribui as devidas posições
+    for (int i = 1; i < padrao->tamanho; i++){
+        tab_desloc[padrao->notas[i-1]] = (padrao->tamanho) - i;
+    }  
+
+    return tab_desloc;
 }
