82705275e27dd259544d00640c1448525593cce6
Luiz_F <103970058+LuizFilipeM@users.noreply.github.com>
2022-06-30 23:15:47 -0300

    Update BMH.c

--- BMH.c
+++ BMH.c
@@ -1,34 +1,34 @@
 #include "BMH.h"
+#define tam_alfabet 12
 
 int BMH(sequencia *arranjo1, sequencia *arranjo2){
-
     if((arranjo1->tamanho == 0) && (arranjo2->tamanho == 0)) return -7;
-    int tabela[12];
+    int tab_desloc[tam_alfabet];
     
-    for (int i = 0; i < 12; i++){
-        tabela[i] = arranjo2->tamanho;
+    for (int i = 0; i < tam_alfabet; i++){
+        tab_desloc[i] = arranjo2->tamanho;
     }
     
     for (int i = 1; i < arranjo2->tamanho; i++){
-        tabela[arranjo2->notas[i-1]] = arranjo2->tamanho - i;
+        tab_desloc[arranjo2->notas[i-1]] = (arranjo2->tamanho) - i;
     }   
     
-    int i = arranjo2->tamanho, j, k;
+    int i = (arranjo2->tamanho), j, k;
     int potencia_ant, potencia_atual;
     k = i;
     j = arranjo2->tamanho;
-        while (i < arranjo1->tamanho)
+        while (i <= arranjo1->tamanho)
         {
-            
+           
             if(comparador(arranjo1->notas[k-1],arranjo2->notas[j-1]) == 0){
                 k = i;
                 j = arranjo2->tamanho;
                 while (arranjo1->notas[k-1] == arranjo2->notas[j-1]){
                     k--;
                     j--;
-                    
+                    if (j == 0) return k;
                 }
-                if (j == 0) return k;
+                
                 
             } else{
                 k = i;
@@ -39,12 +39,14 @@ int BMH(sequencia *arranjo1, sequencia *arranjo2){
                 while (potencia_ant == potencia_atual && j > 0 && potencia_atual != -6){
                     k--;
                     j--;
+                    if (j == 0) return k;
                     potencia_atual = comparador(arranjo1->notas[k-1], arranjo2->notas[j-1]);
                 }
-                if (j == 0) return k;                    
+                                    
             }
 
-            i = i + tabela[arranjo1->notas[i-1]];
+            if (i == (arranjo1->tamanho)) i = 1 + (arranjo1->tamanho); 
+            else i = i + tab_desloc[arranjo1->notas[i-1]];
         }
 
         
